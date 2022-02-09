c0885142d5b3e8f9e20c51968a00215a7c73479e
LuseLars <larsoestmo@gmail.com>
2022-02-09 14:35:20 +0100

    oppdatert unity versjon

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Move : MonoBehaviour
+{
+    public Transform goal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+}
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 installer unity hub
 
-Gå til "installs" og last ned ny editor: "2020.3.27f1"
+Gå til "installs" og last ned ny editor: "2021.2.10f1"
